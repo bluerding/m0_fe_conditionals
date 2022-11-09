11eb8610516beacbcb07bb8bd62eec7d48472e3e
Ben Luerding <bluerding@gmail.com>
2022-11-09 14:33:03 -0600

    finalize comparisons

--- comparisons.js
+++ comparisons.js
@@ -58,21 +58,21 @@ console.log("Is the numberStudents less than or equal to 21?", numberStudents <=
 // Make sure YOU can explain it that way!
 
 console.log(4 < 9);
-// YOU DO: Explain.
+// YOU DO: Explain. The code is asking if the number 4 is less than the number 9. This is true
 
 var books = 3;
 console.log(4 < books);
-// YOU DO: Explain.
+// YOU DO: Explain. The code is asking if the variable set to books is greater than 4. The variable is 3, so it is false
 
 var friends = 6;
 var siblings = 2;
 console.log(friends > siblings);
-// YOU DO: Explain.
+// YOU DO: Explain. The code is asking if the variable set to friends is greater than the variable that is set to siblings. Friends is 6, siblings is 2, so this is true
 
 var attendees = 9;
 var meals = 8;
 console.log(attendees !== meals);
-// YOU DO: Explain.
+// YOU DO: Explain. The code is asking if the number set to var attendees is different than the number set to var meals. They are different, so this is true
 
 
 // #-------------------
@@ -95,15 +95,15 @@ var age = 1;
 
 // YOU DO:
 // Determine if the dog loves to play and loves treats
-
+console.log("The dog loves to play and loves treats", lovesToPlay && lovesTreats)
 
 // Determine if the dog loves to play and loves the dog park
-
+console.log("The dog loves to play and loves the dog park", lovesToPlay && lovesDogPark)
 
 // Determine if the dog loves to play or loves the dog park
-
+console.log("The dog loves to play or loves the dog park", lovesToPlay || lovesDogPark)
 
 // Determine if the dog loves to play and is a puppy
-
+console.log("The dog loves to play and is a puppy", lovesToPlay && age)
 // What did your final line of code evaluate to? Why do you think that is? Explain.
-// ANSWER:
+// ANSWER:The final line gave me the age, 1. I believe this is because that is a number value and not a boolean. If the variable was var puppy = true, then it would be true.
