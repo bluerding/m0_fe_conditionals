02a433a8deab91dee621b1ff90a31259bc58c599
Ben Luerding <bluerding@gmail.com>
2022-11-09 14:46:56 -0600

    finish ifs

--- ifStatements.js
+++ ifStatements.js
@@ -42,16 +42,16 @@
 // Experiment with manipulating the value held within numQuarters
 // to make sure both conditions can be achieved.
 
-var numQuarters = 4
+// var numQuarters = 4
 
 // console.log("I have enough money for a gumball");
 // console.log("I don't have enough money for a gumball");
 
-if (numQuarters <= 1) {
-  console.log("I don't have enough money for a gumball")
-} else if (numQuarters >= 2) {
-  console.log("I have neough money for a gumball")
-}
+// if (numQuarters <= 1) {
+//   console.log("I don't have enough money for a gumball")
+// } else if (numQuarters >= 2) {
+//   console.log("I have neough money for a gumball")
+// }
 
 
 // #####################
@@ -68,6 +68,18 @@ if (numQuarters <= 1) {
 // Experiment with manipulating the value held within both variables
 // to make sure all above conditions output what you expect.
 
-var cupsOfFlour = 1;
+var cupsOfFlour = 3;
 var hasSauce = true;
 
+if (cupsOfFlour === 1 && hasSauce === true){
+  console.log("I cannot make pizza")
+}
+if (cupsOfFlour === 5 && hasSauce === false){
+  console.log("I cannot make pizza")
+}
+if (cupsOfFlour === 2 && hasSauce === true){
+  console.log("I can make pizza")
+}
+if (cupsOfFlour === 3 && hasSauce === true){
+  console.log("I can make pizza")
+}
